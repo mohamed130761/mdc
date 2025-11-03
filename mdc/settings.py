f0ceec4bd59ac483987b989bf33d5a664754afa6
mohamed130761 <mohamed130761@gmail.com>
2025-11-03 19:56:45 +0200

    Initial commit

--- mdc/settings.py
+++ mdc/settings.py
@@ -97,17 +97,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mdc.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.eaftuikwwutnxkylpoan',
+        'PASSWORD': 'P@ssw0rd@200495@',
+        'HOST': 'aws-1-eu-north-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
